b825377053ff27b99a54dda69f31701439530aea
minah <s215722@student.dtu.dk>
2021-10-24 17:04:18 +0200

    board titles

--- src/Matador.java
+++ src/Matador.java
@@ -1,6 +1,16 @@
 public class Matador {
     public static void main (String[] args){
-        
+      String player1name;
+      String player2name;
+      int count=0;
+      String title;
+
+      //making the board
+        int [] fieldNumber= new int[13];
+        while(count<13)
+                title=Integer.toString(fieldNumber[count]);
+        public static void setTitleText(fieldNumber[count], title)
+
     }
 
 }
